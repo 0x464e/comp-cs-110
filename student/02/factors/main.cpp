18e43ecad38486c99b78d5022eac067090efd441
Otto <36742501+0x464e@users.noreply.github.com>
2021-01-26 04:58:23 +0200

    Läheisimmät tekijät

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
+    int dividend;
     cout << "Enter a positive number: ";
+    cin >> dividend;
 
+    if (dividend <= 0)
+    {
+        cout << "Only positive numbers accepted" << endl;
+        return 0;
+    }
+
+    int root = sqrt(dividend);
+    int f1 = root;
+    int f2 = root + (root*root!=dividend);
+
+    for(;;)
+        if (!(dividend % f1))
+            break;
+        else
+            f1--;
+
+    for(;;)
+        if (!(dividend % f2))
+            break;
+        else
+            f2++;
+
+    cout << dividend << " = " << f1 << " * " << f2 << endl;
     return 0;
 }
