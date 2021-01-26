957d3b036a1e63cf32aac57da1683b2c05beca4d
Otto <36742501+0x464e@users.noreply.github.com>
2021-01-26 03:13:43 +0200

    Initial commit

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
-    std::cout << "How many numbers would you like to have? ";
+    int count;
+    cout << "How many numbers would you like to have? ";
+    cin >> count;
+
+    for(int i=1;i<=count;i++)
+        cout << i << endl;
+
+    return 0;
 }
