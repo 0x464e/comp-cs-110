2e87f1001974a7a55db6063ac46f4cabf0da75cb
Otto <36742501+0x464e@users.noreply.github.com>
2021-02-04 17:10:51 +0200

    Add 03 and complete swap excercise

---
+++ student/03/swap/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+
+// Write your swap function here.
+
+
+
+#ifndef UNIT_TESTING
+void swap(int& i, int& j)
+{
+    int i2 = i;
+    i = j;
+    j = i2;
+}
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int i = 0;
+    std::cin >> i;
+
+    std::cout << "Enter another integer: ";
+    int j = 0;
+    std::cin >> j;
+
+    swap(i, j);
+    std::cout << "The integers are " << i << " and " << j << std::endl;
+
+    return EXIT_SUCCESS;
+}
+#endif
