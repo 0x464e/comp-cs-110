d0fe1914c68c32dd603b55c25df60f43ecc009aa
Otto <36742501+0x464e@users.noreply.github.com>
2021-02-10 07:03:54 +0200

    Split assignment

--- student/04/split/main.cpp
+++ student/04/split/main.cpp
@@ -2,10 +2,31 @@
 #include <string>
 #include <vector>
 
+using namespace std;
 
-// TODO: Implement split function here
-// Do not change main function
-
+vector<string> split(const string& line, const char separator, const bool ignore_empty = false)
+{
+    vector<string> parts;
+    string part;
+    for(char c : line)
+    {
+        if(c == separator)
+        {
+            if(part.size())
+                parts.push_back(part);
+            else if (!ignore_empty)
+                parts.push_back("");
+            part = "";
+            continue;
+        }
+        part += c;
+    }
+    if(part.size())
+        parts.push_back(part);
+    else if (!ignore_empty)
+        parts.push_back("");
+    return parts;
+}
 
 int main()
 {
@@ -15,15 +36,13 @@ int main()
     std::cout << "Enter the separator character: ";
     char separator = getchar();
 
-    std::vector< std::string > parts  = split(line, separator);
+    std::vector<std::string> parts = split(line, separator);
     std::cout << "Splitted string including empty parts: " << std::endl;
-    for( auto part : parts ) {
+    for(auto part : parts)
         std::cout << part << std::endl;
-    }
 
-    std::vector< std::string > parts_no_empty  = split(line, separator, true);
+    std::vector<std::string> parts_no_empty = split(line, separator, true);
     std::cout << "Splitted string ignoring empty parts: " << std::endl;
-    for( auto part : parts_no_empty ) {
+    for(auto part : parts_no_empty)
         std::cout << part << std::endl;
-    }
 }
