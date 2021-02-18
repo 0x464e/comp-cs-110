02b82ccbadbdf0c43fa14f0415c1905595c0f4cb
Otto <36742501+0x464e@users.noreply.github.com>
2021-02-18 22:43:14 +0200

    Line numbers assigment

---
+++ student/05/line_numbers/line_numbers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	string input_file;
+	string output_file;
+	cout << "Input file: ";
+	getline(cin, input_file);
+	cout << "Output file: ";
+	getline(cin, output_file);
+
+	ifstream input_file_obj(input_file);
+	ofstream output_file_obj(output_file);
+
+	if (!input_file_obj)
+	{
+		cout << "Error: failed opening the file." << endl;
+		return EXIT_FAILURE;
+	}
+
+	string line;
+	auto i = 0;
+	while (getline(input_file_obj, line))
+		output_file_obj << to_string(++i) << " " << line << endl;
+	input_file_obj.close();
+	output_file_obj.close();
+
+	return EXIT_SUCCESS;
+}
+
